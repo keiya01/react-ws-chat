8cff9c2d73a2189deca3ce442feef39ddc47dac3
keiya01 <keiya.s.0210@gmail.com>
2019-12-28 13:36:50 +0900

    Add chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const App: React.FC = () => {
+  const ws = useRef(new WebSocket("ws://localhost:9000"));
+  const [text, changeText] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeText(e.target.value);
+  }
+  
+  const handleOnSubmit = () => {
+    ws.current.send(text);
+    changeText("");
+  }
+
+  useEffect(() => {
+    ws.current.onmessage = (e: Event) => {
+      const msg = e as Event & {data: string};
+      setMessages(prev => ([
+        ...prev,
+        msg.data
+      ]));
+    };
+  }, []);
+
   return (
     <div>
+      <ul>
+        {messages.map((msg, i) => (
+          <li key={i.toString()}>{msg}</li>
+        ))}
+      </ul>
+      <input value={text} onChange={handleOnChange}/>
+      <button type="button" onClick={handleOnSubmit}>送信</button>
     </div>
   );
 }
